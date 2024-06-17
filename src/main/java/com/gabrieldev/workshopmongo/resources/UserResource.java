8b2a7d9e85122d89f4cd8880818338d6c3f807d3
rampelotti <gabrieelrampelotti@gmail.com>
2024-06-17 18:56:26 -0300

    Obtendo um user por ID e tratando erro

--- src/main/java/com/gabrieldev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrieldev/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.gabrieldev.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabrieldev.workshopmongo.domain.User;
+import com.gabrieldev.workshopmongo.dto.UserDTO;
 import com.gabrieldev.workshopmongo.services.UserService;
 
 @RestController
@@ -17,8 +20,9 @@ public class UserResource {
 
 	@Autowired
 	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		//User maria = new User("1", "Maria Silva", "@maria@gmail.com");
 		//User alex = new User("2", "Alex Brown", "@alex@gmail.com");
 		//List<User> list = new ArrayList<>();
@@ -26,6 +30,16 @@ public class UserResource {
 		//return ResponseEntity.ok().body(list);
 		
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		//cada obj x da lista original, retornar um new userdto, passando o x como arg, depois voltar o stream para lista com o collect.**********
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){	
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+		
+	}
+	
 }
