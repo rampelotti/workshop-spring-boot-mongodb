2eed3062f76bbc97b1350dce380fe1c663d9c14e
rampelotti <gabrieelrampelotti@gmail.com>
2024-06-17 20:29:39 -0300

    Update de User com PUT

--- src/main/java/com/gabrieldev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrieldev/workshopmongo/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,7 +56,6 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		//return void response with a new location
 		return ResponseEntity.created(uri).build();
-		
 	}
 	
 	@DeleteMapping("/{id}")
@@ -63,5 +63,13 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){	
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }
