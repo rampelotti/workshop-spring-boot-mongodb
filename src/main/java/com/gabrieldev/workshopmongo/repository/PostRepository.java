3f930eaf09f0bb2cf610232513a0604e17e1d2af
rampelotti <gabrieelrampelotti@gmail.com>
2024-06-18 20:19:52 -0300

    Consulta simples com @Query

--- src/main/java/com/gabrieldev/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/gabrieldev/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.gabrieldev.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gabrieldev.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.gabrieldev.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	//0 PORQUE É O PRIMEIRO PARAMETRO
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
