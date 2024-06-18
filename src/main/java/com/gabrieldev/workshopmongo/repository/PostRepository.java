477b86a779cda9af119a239df389dfa86955a494
rampelotti <gabrieelrampelotti@gmail.com>
2024-06-18 16:36:10 -0300

    Consulta simples com Query methods

--- src/main/java/com/gabrieldev/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/gabrieldev/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.gabrieldev.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.gabrieldev.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
