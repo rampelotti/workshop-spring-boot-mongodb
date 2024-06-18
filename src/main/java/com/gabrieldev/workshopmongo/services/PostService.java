27c1040c17087a3d88baa5c8bfdfb2312a554ba8
rampelotti <gabrieelrampelotti@gmail.com>
2024-06-17 21:16:39 -0300

    Obtendo Post por Id

---
+++ src/main/java/com/gabrieldev/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.gabrieldev.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrieldev.workshopmongo.domain.Post;
+import com.gabrieldev.workshopmongo.repository.PostRepository;
+import com.gabrieldev.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) { 
+		 Optional<Post> obj = repo.findById(id); 
+		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+		} 
+	
+}
